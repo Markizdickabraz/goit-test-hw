8e990636d8aee3c51443b09df04a079ecaf729be
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-04-22 22:08:24 +0300

    logic setFollower

--- src/components/users/user.js
+++ src/components/users/user.js
@@ -1,48 +1,43 @@
 import { Container, Picture,Logo, AvatarContainer, Line, Tweets, Followers,BtnContainer, BtnFalse, BtnTrue } from "./userStyled";
-import pictureabstraction from '../../Image/picture21.png' // relative path to image 
+import pictureabstraction from '../../Image/picture21.png';
 import logo from '../../Image/Logo.svg';
 import { useState } from "react";
 import axios from "axios";
 
+export default function User({ name, tweets, avatar, followers, id}) {
 
-export default function User({ name, tweets, avatar, followers, id }) {
-    
-async function putUsers() {
-  try {
-    const response = await axios.put(
-      `https://6442af8f76540ce22592fcfe.mockapi.io/users/${id}`,
-      { followers: follower }
-    );
-    console.log(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-}   
-    
-    const [btnFollowing, setBtnFollowing] = useState(false)   
-    const [follower, setFollower] = useState(followers)
-
-    const following = (e) => {
-        if (e.target.nodeName !== 'BUTTON') {
-            return
+    const [btnFollowing, setBtnFollowing] = useState(false);
+    const [follower, setFollower] = useState(followers);
+        
+    async function updateUsers() {
+        try {
+            const response = await axios.put(
+            `https://6442af8f76540ce22592fcfe.mockapi.io/users/${id}`,
+            {followers: follower}
+            );
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
         }
+    };
+
+     function following() {
+        // if (e.target.nodeName !== 'BUTTON') {
+        //     return;
+        // }
         if (btnFollowing) {
             setBtnFollowing(false)
             setFollower(follower - 1)
-            putUsers(e.currentTarget.id )
-        }
-
+        }  
         if (!btnFollowing) {
             setBtnFollowing(true)
             setFollower(follower + 1)
-            putUsers(e.currentTarget.id)
         }
-        console.log(follower)
-        console.dir(e.currentTarget)
-    }
-
+            updateUsers()
+    };
+    
     return (
-        <Container key={id} id={id}  onClick={following}>
+        <Container key={id} id={id}>
             <Logo src={logo} alt='logo' />
             <Picture src={pictureabstraction} alt="pictureabstraction" />
             <AvatarContainer>
@@ -52,11 +47,10 @@ async function putUsers() {
             <Tweets>{tweets} Tweets</Tweets>
             <Followers>{follower} followers</Followers>
             <BtnContainer>
-                {!btnFollowing && <BtnFalse type="button">follow</BtnFalse>}
-                {btnFollowing && <BtnTrue type="button">following</BtnTrue>}
-                
+                {!btnFollowing && <BtnFalse type="button" onClick={following}>follow</BtnFalse>}
+                {btnFollowing && <BtnTrue type="button" onClick={following}>following</BtnTrue>}
             </BtnContainer>
-            </Container>
+        </Container>
     )
 }
 
